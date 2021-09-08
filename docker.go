a0fb2e988014f9c4a98ca9217bc046ff455ff33b
Vladimir Romanko <vladimir.romanko@kaspersky.com>
2021-09-08 18:14:25 +0300

    bugfix for: panic: received invalid log type: 3

--- docker.go
+++ docker.go
@@ -395,17 +395,13 @@ func (c *DockerContainer) StartLogProducer(ctx context.Context) error {
 				return
 			default:
 				h := make([]byte, 8)
-				_, err := r.Read(h)
+				_, err := io.ReadFull(r, h)
 				if err != nil {
-					// proper type matching requires https://go-review.googlesource.com/c/go/+/250357/ (go 1.16)
-					if strings.Contains(err.Error(), "use of closed connection") {
-						now := time.Now()
-						since = fmt.Sprintf("%d.%09d", now.Unix(), int64(now.Nanosecond()))
-						goto BEGIN
-					}
 					// this explicitly ignores errors
 					// because we want to keep procesing even if one of our reads fails
-					continue
+					now := time.Now()
+					since = fmt.Sprintf("%d.%09d", now.Unix(), int64(now.Nanosecond()))
+					goto BEGIN
 				}
 
 				count := binary.BigEndian.Uint32(h[4:])
@@ -421,11 +417,16 @@ func (c *DockerContainer) StartLogProducer(ctx context.Context) error {
 				logTypes := []string{"", StdoutLog, StderrLog}
 
 				b := make([]byte, count)
-				_, err = r.Read(b)
+				_, err = io.ReadFull(r, b)
 				if err != nil {
 					// TODO: add-logger: use logger to log out this error
 					fmt.Fprintf(os.Stderr, "error occurred reading log with known length %s", err.Error())
-					continue
+
+					// this explicitly ignores errors
+					// because we want to keep procesing even if one of our reads fails
+					now := time.Now()
+					since = fmt.Sprintf("%d.%09d", now.Unix(), int64(now.Nanosecond()))
+					goto BEGIN
 				}
 				for _, c := range c.consumers {
 					c.Accept(Log{
